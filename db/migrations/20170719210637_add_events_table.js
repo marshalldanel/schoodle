426d2e5b18e2ace5c73ea635ae7bd3e952a5aa61
Cameron Cutler <cameron.t.cutler@gmail.com>
2017-07-19 21:28:41 +0000

    Changed it so the tables are done through knex migrations

---
+++ db/migrations/20170719210637_add_events_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('events', function (table) {
+    table.increments('id')
+    table.string('title')
+    table.string('description')
+    table.integer('admin_id').unsigned()
+    table.foreign('admin_id').references('admins.id')
+    table.string('event_date', 10)
+    table.string('location')
+    table.string('event_code', 8)
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('admins');
+};

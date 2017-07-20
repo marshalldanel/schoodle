871c8e8f770216cc2f9b6b124ca7c1cbeaafa0e2
marshalldanel <marshalldanel@gmail.com>
2017-07-20 20:56:43 +0000

    Updated to add routes

---
+++ routes/new.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+const randStr = require('../public/scripts/makeCode');
+
+module.exports = (knex) => {
+
+  router.get('/event/new', (req, res) => {
+    res.render('new');
+  });
+
+  router.post('/event/new', (req, res) => {
+    let randAdmin = randStr();
+    let randEvent = randStr();
+    knex.transaction(() => {
+      return knex('admins')
+        .insert({
+          name: req.body.admin_name,
+          email: req.body.admin_email,
+          admin_code: randAdmin
+        })
+        .returning('*')
+        .then(([admin]) => {
+          return knex('events')
+            .insert({
+              title: req.body.title,
+              location: req.body.location,
+              description: req.body.description,
+              event_date: req.body.date,
+              event_code: randEvent,
+              admin_id: admin.id
+            });
+        })
+        .then(function () {
+          res.json({ success: true, message: 'ok' });
+        });
+    });
+  });
+
+  return router;
+};
